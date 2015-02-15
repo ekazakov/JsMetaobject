2367237128b2469c0e8a8a8a967d007ee59fd16a
Evgeniy Kazakov <evgeniy.kazakov@gmail.com>
2015-02-16 02:10:44 +0400

    Up

--- private-meta-objects.js
+++ private-meta-objects.js
@@ -1,43 +1,3 @@
-var Person = {
-    fullName: function () {
-        return this.firstName + " " + this.lastName;
-    },
-    rename: function (first, last) {
-        this.firstName = first;
-        this.lastName = last;
-        return this;
-    }
-};
-
-var HasName = {
-  name: function () {
-    return this.name;
-  },
-  setName: function (name) {
-    this.name = name;
-    return this;
-  }
-};
-
-var IsSelfDescribing = {
-  description: function () {
-    return this.name() + ' is a ' + this.career();
-  }
-};
-
-var HasCareer = {
-    career: function () {
-        return this.chosenCareer;
-    },
-    setCareer: function (career) {
-        this.chosenCareer = career;
-        return this;
-    },
-    describe: function () {
-        return this.fullName() + " is a " + this.chosenCareer;
-    }
-};
-
 var __slice = Array.prototype.slice;
 
 function extend () {
@@ -58,17 +18,23 @@ function extend () {
     return melded;
 }
 
-function proxy (baseObject, optionalPrototype) {
+function methodsOfType (behaviour, type) {
+    return Object.keys(behaviour).filter(function (methodName) {
+        return typeof behaviour[methodName] === type;
+    });
+}
+
+function proxy (baseObject, methods, optionalPrototype) {
     var proxyObject = Object.create(optionalPrototype || null);
 
-    Object.keys(baseObject).forEach(function (methodName) {
-        if (typeof baseObject[methodName] === 'function') {
-            proxyObject[methodName] = function () {
-                var result = baseObject[methodName].apply(baseObject, arguments);
+    methods = methods || methodsOfType(baseObject, 'function');
 
-                return result === baseObject ? proxyObject : result;
-            };
-        }
+    methods.forEach(function (methodName) {
+        proxyObject[methodName] = function () {
+            var result = baseObject[methodName].apply(baseObject, arguments);
+
+            return result === baseObject ? proxyObject : result;
+        };
     });
 
     return proxyObject;
@@ -79,13 +45,31 @@ var privateStateId = 0;
 function extendWithProxy (baseObject, behaviour) {
     var safekeepingName = '__' + (++privateStateId) + '__';
 
+    var methods = methodsOfType(behaviour, 'function');
+    var dependencies = methodsOfType(behaviour, 'undefined');
+
+    var privateMethods = methods.filter(function (methodName) {
+        return methodName[0] === '_';
+    });
+
+    var publicMethods = methods.filter(function (methodName) {
+        return methodName[0] !== '_';
+    });
+
     function createContext (methodReceiver) {
-        return Object.defineProperty(proxy(methodReceiver), 'self', {
+        var proto = privateMethods.reduce(function (acc, methodName) {
+            acc[methodName] = behaviour[methodName];
+
+            return acc;
+        }, {});
+
+        var innerProxy = proxy(methodReceiver, dependencies, proto);
+
+        return Object.defineProperty(innerProxy, 'self', {
             writable: false,
             enumerable: false,
             value: methodReceiver
         });
-        // return proxy(methodReceiver);
     }
 
     function getContext (methodReceiver) {
@@ -101,26 +85,25 @@ function extendWithProxy (baseObject, behaviour) {
         return context;
     }
 
-    Object.keys(behaviour).forEach(function (methodName) {
+    return publicMethods.reduce(function (acc, methodName) {
         var methodBody = behaviour[methodName];
 
-        baseObject[methodName] = function () {
+        acc[methodName] = function () {
             var context = getContext(this);
-            var result;
+            var result = methodBody.apply(context, arguments);
 
-            result = methodBody.apply(context, arguments);
             return (result === context) ? this : result;
         };
-    });
 
-    return baseObject;
+        return acc;
+    }, {});
 }
 
 function encapsulate (behaviour) {
     return extendWithProxy(Object.create(null), behaviour);
 }
 
-Person = encapsulate({
+var Person = encapsulate({
     fullName: function () {
         return this.firstName + " " + this.lastName;
     },
@@ -131,7 +114,7 @@ Person = encapsulate({
     }
 });
 
-HasCareer = encapsulate({
+var HasCareer = encapsulate({
     career: function () {
         return this.chosenCareer;
     },
@@ -139,7 +122,51 @@ HasCareer = encapsulate({
         this.chosenCareer = career;
         return this;
     },
-    describe: function () {
-        return this.fullName() + " is a " + this.chosenCareer;
+    // describe: function () {
+        // return this.fullName() + " is a " + this.chosenCareer;
+    // }
+});
+
+var HasName = encapsulate({
+    setName: function (name) {
+        this._name = name;
+    },
+
+    name: function () {
+        return this._name;
     }
+});
+
+
+
+
+var IsSelfDescribing = encapsulate({
+    name: undefined,
+    career: undefined,
+
+    description: function () {
+        return this.name() + ' is a ' + this.career();
+    }
+});
+
+
+var MultiTalented = encapsulate({
+  _englishList: function (list) {
+    var butLast = list.slice(0, list.length - 1),
+        last = list[list.length - 1];
+    return butLast.length > 0
+           ? [butLast.join(', '), last].join(' and ')
+           : last;
+  },
+  constructor: function () {
+    this._careers = [];
+    return this;
+  },
+  addCareer: function (career) {
+    this._careers.push(career);
+    return this;
+  },
+  careers: function () {
+    return this._englishList(this._careers);
+  }
 });
